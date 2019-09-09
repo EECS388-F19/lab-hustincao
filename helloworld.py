c91b52c281e42c9f24b434acc2981ed4b2c7a969
hustin <hustin@umich.edu>
2019-09-09 15:48:14 -0400

    initial push

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+from random import randint
+a = randint(1,100)
+b = randint(1,100)
+result = a+b
+print ("Hustin")
+print (a)
+print (b)
+print ("Sum = " + result))
+print ("Average = " + str(result/2.0))
---
+++ helloworld2.py
@@ -0,0 +1,9 @@
+mylist = ["Calen", "Hustin", "Kayla"]
+mylist.sort()
+
+first_name = list[0]
+first_name = first_name[1:]
+
+print('Sorted list:', vowels)
+print(first_name)
+print max(mylist, key=len)
